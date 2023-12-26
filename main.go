cfac4ba0a2d02aa0d4cfaae1ec17cbcea4edd298
tanerijun <tanerivince@gmail.com>
2023-12-26 23:24:27 +0800

    Refactor and more routes

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to Snip-Snap"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("TODO: display snippets"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("TODO: UI to create snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to Snip-Snap"))
-	})
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Server started on port 4000")
 	err := http.ListenAndServe(":4000", mux)
