3ec9e087d8f77e2dfb17605b75ad57bd5b040cf9
tanerijun <tanerivince@gmail.com>
2023-12-29 12:13:15 +0800

    Create custom loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,6 +13,10 @@ func main() {
 	staticDir := flag.String("static-dir", "./ui/static", "Path to static assets")
 
 	flag.Parse()
+
+	// Logger
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// HTTP
 	mux := http.NewServeMux()
@@ -23,7 +28,13 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Server started on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Server started on %s", *addr)
+	err := server.ListenAndServe()
+	errorLog.Fatal(err)
 }
