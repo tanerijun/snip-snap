00fd55afa7df183044b9706b5fc134f31794a967
tanerijun <tanerivince@gmail.com>
2024-01-01 13:39:15 +0800

    Connect to DB

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // Handler dependencies
@@ -16,8 +19,24 @@ type application struct {
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	staticDir := flag.String("static-dir", "./ui/static", "Path to static assets")
+	dsn := flag.String("dsn", "", "PostgreSQL data source name (required)")
 
 	flag.Parse()
+	if *dsn == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	dbpool, err := pgxpool.New(context.Background(), *dsn)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer dbpool.Close()
+
+	err = dbpool.Ping(context.Background())
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
@@ -36,6 +55,6 @@ func main() {
 	}
 
 	infoLog.Printf("Server started on %s", *addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
 }
---
+++ migrations/00.init.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE snip_snap
+  ENCODING 'UTF8'
+  LC_COLLATE = 'en_US.UTF-8'
+  LC_CTYPE = 'en_US.UTF-8';
+
+CREATE TABLE snippets (
+  id SERIAL,
+  title VARCHAR(100) NOT NULL,
+  content TEXT NOT NULL,
+  created TIMESTAMPTZ NOT NULL,
+  expires TIMESTAMPTZ NOT NULL
+);
+
+CREATE INDEX idx_snippets_created ON snippets(created);
+
+INSERT INTO snippets (title, content, created, expires) VALUES (
+  'An old silent pond',
+  'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō', 
+  NOW(),
+  NOW() + INTERVAL '365 days'
+);
+INSERT INTO snippets (title, content, created, expires) VALUES (
+  'Over the wintry forest',
+  'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki', 
+  NOW(),
+  NOW() + INTERVAL '365 days'
+);
+INSERT INTO snippets (title, content, created, expires) VALUES (
+  'First autumn morning',
+  'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo', 
+  NOW(),
+  NOW() + INTERVAL '365 days'
+);
