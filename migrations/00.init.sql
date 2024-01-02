7f708b67619da5c38eda64ee1732622ce7101329
tanerijun <tanerivince@gmail.com>
2024-01-02 23:47:45 +0800

    Fix escape character issue with Postgres

--- migrations/00.init.sql
+++ migrations/00.init.sql
@@ -15,19 +15,19 @@ CREATE INDEX idx_snippets_created ON snippets(created);
 
 INSERT INTO snippets (title, content, created, expires) VALUES (
   'An old silent pond',
-  'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō', 
+  E'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō', 
   NOW(),
   NOW() + INTERVAL '365 days'
 );
 INSERT INTO snippets (title, content, created, expires) VALUES (
   'Over the wintry forest',
-  'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki', 
+  E'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki', 
   NOW(),
   NOW() + INTERVAL '365 days'
 );
 INSERT INTO snippets (title, content, created, expires) VALUES (
   'First autumn morning',
-  'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo', 
+  E'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo', 
   NOW(),
   NOW() + INTERVAL '365 days'
 );
