9e74fcbebee4aff1b738e9bf92d9678a196f1d06
tanerijun <tanerivince@gmail.com>
2024-01-13 22:31:52 +0800

    Update nav bar to reflect auth state

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -32,8 +32,9 @@ func (app *application) notFound(w http.ResponseWriter) {
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -76,4 +77,8 @@ func (app *application) decodePostForm(r *http.Request, destination any) error {
 	}
 
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
