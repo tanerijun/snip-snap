0129f0bc4773b6881008a3bd0b5ee0fe0893980c
tanerijun <tanerivince@gmail.com>
2024-01-16 10:14:28 +0800

    Convert PermittedInt validator to user generics

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -65,8 +65,8 @@ func Matches(value string, rx *regexp.Regexp) bool {
 	return rx.MatchString(value)
 }
 
-// PermittedInt() returns true if a value is in a list of permitted integers.
-func PermittedInt(value int, permittedValues ...int) bool {
+// PermittedValue() returns true if a value is in a list of permitted values.
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	for i := range permittedValues {
 		if value == permittedValues[i] {
 			return true
