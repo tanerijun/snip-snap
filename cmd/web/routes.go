48d40aba683ec8413d82d991765b711cfe9f24d1
tanerijun <tanerivince@gmail.com>
2024-01-16 10:02:26 +0800

    Implement embedded file systems, and serve static files from there

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"github.com/tanerijun/snip-snap/ui"
 )
 
 func (app *application) routes(staticDir string) http.Handler {
@@ -14,8 +15,8 @@ func (app *application) routes(staticDir string) http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir(staticDir))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	dynamicMiddlewares := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
