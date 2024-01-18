e1443e350baed976dc8fa84d9b37943c0cb79e9d
tanerijun <tanerivince@gmail.com>
2024-01-18 20:12:01 +0800

    Properly redirect unauthenticated user

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -44,6 +44,9 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 func (app *application) requireAuthentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !app.isAuthenticated(r) {
+			// Store the url where user is coming from
+			app.sessionManager.Put(r.Context(), "redirectPathAfterLogin", r.URL.Path)
+
 			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 			return
 		}
